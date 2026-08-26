ca833dd6c22ab591a46561c6c37c863fe51a6cb9
Abhay Chauhan <abhaychauhanchd2@gmail.com>
2026-08-26 19:29:43 +0530

    Time: 7 ms (72.80%) | Memory: 49.7 MB (44.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
@@ -48,5 +49,29 @@ class Solution {
         }
 
         return result;
+    }
+}
+*/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+
+            String key = new String(ch);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
     }
 }
